4a57fee7f5aed9fc11df7fefc059eca068eacebc
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-08-25 23:12:21 +0530

    Update check_and_alert.py

--- check_and_alert.py
+++ check_and_alert.py
@@ -1,7 +1,15 @@
+from classify_temperature_breach import classify_temperature_breach
+from send_alert import send_to_controller,send_to_email
+
+
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+  breachType = classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+    
   if alertTarget == 'TO_CONTROLLER':
     send_to_controller(breachType)
   elif alertTarget == 'TO_EMAIL':
     send_to_email(breachType)
+  else:
+    raise ValueError(f"Unknown alert target: {alertTarget}")
+    
+  print(f"Alert sent to {alertTarget}: {breachType}")
