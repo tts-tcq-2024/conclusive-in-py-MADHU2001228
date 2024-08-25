578435a0ba75823e6f8a4d4963dffdc6f8ad8ac5
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-08-25 23:25:07 +0530

    Update alert.test.py

--- alert.test.py
+++ alert.test.py
@@ -1,14 +1,42 @@
 import unittest
-from classify_temperature_breach import classify_temperature_breach
 from infer_breach import infer_breach
-from check_and_alert import check_and_alert
-import typewise_alert
 
-
-class TypewiseTest(unittest.TestCase):
-  def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-
+class TestInferBreach(unittest.TestCase):
+    
+    def test_too_low(self):
+        self.assertEqual(infer_breach(20, 50, 100), 'TOO_LOW')
+        
+    def test_too_high(self):
+        self.assertEqual(infer_breach(110, 50, 100), 'TOO_HIGH')
+        
+    def test_normal(self):
+        self.assertEqual(infer_breach(75, 50, 100), 'NORMAL')
+        
+    def test_at_lower_limit(self):
+        self.assertEqual(infer_breach(50, 50, 100), 'NORMAL')
+        
+    def test_at_upper_limit(self):
+        self.assertEqual(infer_breach(100, 50, 100), 'NORMAL')
+        
+    def test_below_lower_limit(self):
+        self.assertEqual(infer_breach(49, 50, 100), 'TOO_LOW')
+        
+    def test_above_upper_limit(self):
+        self.assertEqual(infer_breach(101, 50, 100), 'TOO_HIGH')
+        
+    def test_edge_case_lower(self):
+     
+        self.assertEqual(infer_breach(0, 0, 0), 'NORMAL')
+        
+    def test_edge_case_upper(self):
+       
+        self.assertEqual(infer_breach(0, 0, 0), 'NORMAL')
+        
+    def test_negative_temperature(self):
+        self.assertEqual(infer_breach(-10, -20, -5), 'NORMAL')
+        self.assertEqual(infer_breach(-30, -20, -5), 'TOO_LOW')
+        self.assertEqual(infer_breach(0, -20, -5), 'TOO_HIGH')
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
+
