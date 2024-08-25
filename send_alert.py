0c8851cdbe98a093f17ce8eea2d6775cec3263ae
MADHU2001228 <82486243+MADHU2001228@users.noreply.github.com>
2024-08-25 22:49:44 +0530

    Create send_alert.py

---
+++ send_alert.py
@@ -0,0 +1,12 @@
+def send_to_controller(breachType):
+  header = 0xfeed
+  print(f'{header}, {breachType}')
+
+def send_to_email(breachType):
+  recepient = "a.b@c.com"
+  if breachType == 'TOO_LOW':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too low')
+  elif breachType == 'TOO_HIGH':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too high')
